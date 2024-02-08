e9e123d3728f7dd380aebc6c6f049d2860fa64b2
pavanprajapati62 <pavanprajapati62@gmail.com>
2024-02-08 10:15:47 +0530

    check

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('userLogin', [UserLoginController::class, 'loginapi']);
+
 Route::get('getProfile', [ApiController::class, 'getProfile']);
 
