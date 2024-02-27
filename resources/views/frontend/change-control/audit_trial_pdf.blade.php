74c42d8170e29272cebaee3c7feba58f1356a5dd
Yugal-123 <blackgrapes.developers2@gmail.com>
2024-02-28 02:04:54 +0530

    Changes in CC-Audit_trial

--- resources/views/frontend/change-control/audit_trial_pdf.blade.php
+++ resources/views/frontend/change-control/audit_trial_pdf.blade.php
@@ -166,10 +166,10 @@
                     <strong>Change Control No.</strong>
                 </td>
                 <td class="w-40">
-                   {{ Helpers::getDivisionName($doc->division_id) }}/CC/{{ Helpers::year($doc->created_at)}}/{{ Helpers::record($doc->record) }}
+                   {{ Helpers::getDivisionName($doc->division_id) }}/CC/{{ Helpers::year($doc->created_at)}}/ {{ str_pad($doc->record, 4, '0', STR_PAD_LEFT) }}
                 </td>
                 <td class="w-30">
-                    <strong>Record No.</strong> {{ Helpers::record($doc->record) }}
+                    <strong>Record No.</strong> {{ str_pad($doc->record, 4, '0', STR_PAD_LEFT) }}
                 </td>
             </tr>
         </table>
