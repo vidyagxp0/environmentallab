3d6bcd4957c214d3eb3d25be6b7e1aa89a29ad23
soni2904 <159138191+bgsdevelopers2@users.noreply.github.com>
2024-02-29 10:42:55 +0530

    changes for record no.

--- resources/views/frontend/riskAssesment/audit-trail.blade.php
+++ resources/views/frontend/riskAssesment/audit-trail.blade.php
@@ -10,8 +10,8 @@
                 <div class="inner-block">
                     <div class="main-head">
                         <div class="default-name">{{ date('Y') }}
-                            /Record-000{{ $document->id }}</div>
-
+                            /    {{ Helpers::getDivisionName(session()->get('division')) }}/LI/{{ date('Y') }}/{{ str_pad($document->record, 4, '0', STR_PAD_LEFT) }}
+                         </div>
                             <div class="btn-group">
                                 <a href="{{ url('rcms/riskAuditReport',$document->id)}}"> <button type="button">Print</button></a>
                             </div>
