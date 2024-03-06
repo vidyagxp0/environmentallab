3699ef2be8254852f306bbde8fd34c07163273d3
preetamdigital <preetam.patel@lifelinkdigital.com>
2024-03-07 00:08:34 +0530

    chart

--- resources/views/frontend/analytics.blade.php
+++ resources/views/frontend/analytics.blade.php
@@ -240,64 +240,11 @@
                          
                         </div>
 
-                        <script>
-                        function fetchData(selectedValue) {
-                         fetch(`/chart-data?value=${selectedValue}`)
-                            .then(response => response.json())
-                            .then(data => {
-                                var options = {
-                                    series: [{
-                                        name: 'Total',
-                                        data: data.map(item => item.value),
-                                        // Define color for each category
-                                        colors: ['#008FFB', '#00E396', '#FEB019', '#FF4560']
-                                    }],
-                                    chart: {
-                                        type: 'bar',
-                                        height: 350,
-                                        stacked: true,
-                                        toolbar: {
-                                            show: true
-                                        },
-                                        zoom: {
-                                            enabled: true
-                                        }
-                                    },
-                                    plotOptions: {
-                                        bar: {
-                                            horizontal: false,
-                                            borderRadius: 10,
-                                            dataLabels: {
-                                                total: {
-                                                    enabled: true,
-                                                    style: {
-                                                        fontSize: '13px',
-                                                        fontWeight: 900
-                                                    }
-                                                }
-                                            }
-                                        },
-                                    },
-                                    xaxis: {
-                                        type: 'category',
-                                        categories: data.map(item => item.division)
-                                    },
-                                    legend: {
-                                        position: 'right',
-                                        offsetY: 40
-                                    },
-                                    fill: {
-                                        opacity: 1
-                                    }
-                                };
-
-                                var chart = new ApexCharts(document.querySelector("#chart"), options);
-                                chart.render();
-                            });
-                        }
-                            </script>
+                    
                         </div>
-
+                        <div id="new-chart-id">
+                            <h4>Status</h4>
+                        </div>
                         </div>
                      {{-- <div class="scope-pagination">
                             {{ $datag->links() }}
@@ -393,6 +340,134 @@ function showChart() {
     fetchData(selectedValue);
 }
     </script>
+
+<script>
+    function fetchData(selectedValue) {
+     fetch(`/chart-data?value=${selectedValue}`)
+        .then(response => response.json())
+        .then(data => {
+            var options = {
+                series: [{
+                    name: 'Total',
+                    data: data.map(item => item.value),
+                    // Define color for each category
+                    colors: ['#008FFB', '#00E396', '#FEB019', '#FF4560']
+                }],
+                chart: {
+                    type: 'bar',
+                    height: 350,
+                    stacked: true,
+                    toolbar: {
+                        show: true
+                    },
+                    zoom: {
+                        enabled: true
+                    }
+                },
+                plotOptions: {
+                    bar: {
+                        horizontal: false,
+                        borderRadius: 10,
+                        dataLabels: {
+                            total: {
+                                enabled: true,
+                                style: {
+                                    fontSize: '13px',
+                                    fontWeight: 900
+                                }
+                            }
+                        }
+                    },
+                },
+                xaxis: {
+                    type: 'category',
+                    categories: data.map(item => item.division)
+                },
+                legend: {
+                    position: 'right',
+                    offsetY: 40
+                },
+                fill: {
+                    opacity: 1
+                }
+            };
+
+            var chart = new ApexCharts(document.querySelector("#chart"), options);
+            chart.render();
+        });
+    }
+        </script>
+
+
+<script>
+    var options = {
+      series: [{
+        name: 'PRODUCT A',
+        data: [44, 55, 41, 67, 22, 43]
+      }, {
+        name: 'PRODUCT B',
+        data: [13, 23, 20, 8, 13, 27]
+      }, {
+        name: 'PRODUCT C',
+        data: [11, 17, 15, 15, 21, 14]
+      }, {
+        name: 'PRODUCT D',
+        data: [21, 7, 25, 13, 22, 8]
+      }],
+      chart: {
+        type: 'bar',
+        height: 350,
+        stacked: true,
+        toolbar: {
+          show: true
+        },
+        zoom: {
+          enabled: true
+        }
+      },
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          legend: {
+            position: 'bottom',
+            offsetX: -10,
+            offsetY: 0
+          }
+        }
+      }],
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          borderRadius: 10,
+          dataLabels: {
+            total: {
+              enabled: true,
+              style: {
+                fontSize: '13px',
+                fontWeight: 900
+              }
+            }
+          }
+        },
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
+          '01/05/2011 GMT', '01/06/2011 GMT'
+        ],
+      },
+      legend: {
+        position: 'right',
+        offsetY: 40
+      },
+      fill: {
+        opacity: 1
+      }
+    };
+  
+    var chart = new ApexCharts(document.querySelector("#new-chart-id"), options);
+    chart.render();
+  </script>
        <style>
 #chart {
     display: none; 
