33c20be82b70c8e14d956932f0efdf3a8e3a691a
Aditya Rajput <blackgrapes.developers@gmail.com>
2024-03-08 18:33:20 +0530

    change in Document Seeder

--- database/seeders/DocumentSeeder.php
+++ database/seeders/DocumentSeeder.php
@@ -48,14 +48,62 @@ class DocumentSeeder extends Seeder
 
         $department  = new DocumentType();
         $department->name = "Process Flow Diagram   ";
-        $department->departmentid = 5;
+        $department->departmentid = 6;
         $department->typecode = "PFD";
         $department->save();
 
         $department  = new DocumentType();
         $department->name = "Qualification Protocol   ";
-        $department->departmentid = 5;
+        $department->departmentid = 7;
         $department->typecode = "QP";
         $department->save();
+
+        $department  = new DocumentType();
+        $department->name = "Standard Operation Procedure for Microbiology   ";
+        $department->departmentid = 8;
+        $department->typecode = "SOP-M";
+        $department->save();
+
+        $department  = new DocumentType();
+        $department->name = "Standard Operation Procedure for Chemistry/Wet Chemistry   ";
+        $department->departmentid = 9;
+        $department->typecode = "SOP-C";
+        $department->save(); 
+        
+        $department  = new DocumentType();
+        $department->name = "Standard Operation Procedure for Instrumental/Analytical Tests   ";
+        $department->departmentid = 10;
+        $department->typecode = "SOP-A";
+        $department->save(); 
+
+        $department  = new DocumentType();
+        $department->name = "Standard Operation Procedure for Equipment/ Instruments SOP   ";
+        $department->departmentid = 11;
+        $department->typecode = "SOP-E";
+        $department->save(); 
+
+        $department  = new DocumentType();
+        $department->name = "Quality Policies   ";
+        $department->departmentid = 12;
+        $department->typecode = "QP";
+        $department->save(); 
+
+        $department  = new DocumentType();
+        $department->name = "Method Validation    ";
+        $department->departmentid = 13;
+        $department->typecode = "MV";
+        $department->save(); 
+
+        $department  = new DocumentType();
+        $department->name = "Validation Protocol   ";
+        $department->departmentid = 14;
+        $department->typecode = "VP";
+        $department->save(); 
+
+        $department  = new DocumentType();
+        $department->name = "Electron   ";
+        $department->departmentid = 15;
+        $department->typecode = "EIR";
+        $department->save(); 
     }
 }
