c407a6bf456824a1b4e1a3fbc3470240113f9e76
Pavan Prajapati <pavanprajapati62@gmail.com>
2024-03-09 08:08:05 +0530

    role name show

--- resources/views/admin/account/account.blade.php
+++ resources/views/admin/account/account.blade.php
@@ -38,36 +38,24 @@
                         <tbody>
 
                             @foreach ($users as $user)
+                            
                             <tr>
-                                @php
-                                // Fetch role_ids associated with the user
-                                $hodUserList = DB::table('user_roles')->where('user_id', $user->id)->pluck('role_id')->toArray();
-                                
-                                @endphp
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>{{ $user->dname }}</td>
-                                <td> {{ implode(',',$hodUserList) }}
-                                   
-                                </td>
-                                <th>
-                                <a class="mdi mdi-table-edit"
-                                href="{{ route('user_management.edit', $user->id) }}"><button
-                                    class="btn btn-dark">Edit</button></a>
-
-                            <form action="{{ route('user_management.destroy', $user->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-
-                                <button type="submit" class="confirmation btn btn-danger">Delete</button>
-                            </form>
-                               </th>
-                            </tr>
-                                {{-- <tr>
+                                    @php
+                                    $RoleList = DB::table('user_roles')->where(['user_id' =>$user->id])->pluck('role_id')->toArray();
+                                    $role = '';
+                                    $roleName = '';
+                                    if($RoleList){
+                                        $role = implode(',', $RoleList);
+                                        $roleNameList = DB::table('q_m_s_roles')
+                                            ->whereIn('id', $RoleList)
+                                            ->pluck('name')->toArray();
+                                        $roleName = implode(',', $roleNameList);
+                                    }
+                                    @endphp
                                     <td>{{ $user->name }}</td>
                                     <td>{{ $user->email }}</td>
                                     <td>{{ $user->dname }}</td>
-                                    <th>{{ $user->role }}</th>
+                                    <th>{{ $roleName }}</th>
                                     <td>
                                         <a class="mdi mdi-table-edit"
                                             href="{{ route('user_management.edit', $user->id) }}"><button
@@ -80,7 +68,7 @@
                                             <button type="submit" class="confirmation btn btn-danger">Delete</button>
                                         </form>
                                     </td>
-                                </tr> --}}
+                                </tr>
                             @endforeach
 
                             </tfoot>
