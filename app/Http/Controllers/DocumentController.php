ad609bb4ab182c8a0a1efa3efc0198421b0c6316
gautamsolankigxp <166195389+gautamsolankigxp@users.noreply.github.com>
2024-04-28 17:29:59 +0530

    chng

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -1978,7 +1978,7 @@ class DocumentController extends Controller
             $newdoc->revision_summary = $document->revision_summary;
             $newdoc->training_required = $document->training_required;
             $newdoc->trainer = $request->trainer;
-            $newdoc->document_number = $request->document_number;
+            $newdoc->document_number = $document->document_number;
             $newdoc->comments = $request->comments;
             //$newdoc->purpose = $request->purpose;
             $newdoc->stage = 1;
