3782476671f4d2f68fc802a5377caf3d9182857f
Harsh merchant <harshmerchant2000@gmail.com>
2024-09-10 14:02:11 +0530

    question.blade file working

--- resources/views/frontend/TMS/question.blade.php
+++ resources/views/frontend/TMS/question.blade.php
@@ -63,21 +63,31 @@
                                                     @endif
                                                 </ul>
                                             </td>
-                                            <td>
-                                                <ul>
-                                                    @if (!empty($temp->answers))
-                                                        @foreach (unserialize($temp->answers) as $option)
-                                                            <li>
-                                                                @if (is_numeric($option))
-                                                                    {{ $loop->index + 1 }}
-                                                                @else
-                                                                    {{ $option }}
-                                                                @endif
-                                                            </li>
-                                                        @endforeach
-                                                    @endif
-                                                </ul>
-                                            </td>
+                               <td>
+                                        <ul>
+                                            @if (!empty($temp->answers))
+                                                @php
+                                                    $answers = unserialize($temp->answers);
+                                                @endphp
+                                                @if (is_array($answers) || is_object($answers))
+                                                    @foreach ($answers as $option)
+                                                        <li>
+                                                            @if (is_numeric($option))
+                                                                {{ $loop->index + 1 }}
+                                                            @else
+                                                                {{ $option }}
+                                                            @endif
+                                                        </li>
+                                                    @endforeach
+                                                @else
+                                                    <li>No valid answers available</li>
+                                                @endif
+                                            @else
+                                                <li>No answers available</li>
+                                            @endif
+                                        </ul>
+                                    </td>
+
                                             <td>{{ $temp->type }}</td>
                                             <td>
                                                 <div class="action-btns">
@@ -144,92 +154,7 @@
     {{-- ======================================
                 ADD QUESTION MODAL
     ======================================= --}}
-    <!-- <div class="modal fade" id="add-question-modal">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-
-                <div class="modal-header">
-                    <h4 class="modal-title">Add Question</h4>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                </div>
-                <form action="{{ route('question.store') }}" method="post">
-                    @csrf
-                    <div class="modal-body">
-                        <div class="group-input">
-                            <label for="ques-type">Question Type</label>
-                            <select name="type" id="questionType" onchange="handleChange(this.value)">
-                                <option value="">---</option>
-                                <option value="Single Selection Questions"
-                                    @if (old('type') == 'Single Selection Questions') selected @endif>Single Selection Questions</option>
-                                <option value="Multi Selection Questions"@if (old('type') == 'Multi Selection Questions') selected @endif>
-                                    Multi Selection Questions</option>
-                                {{-- <option value="Exact Match Questions"@if (old('type') == 'Exact Match Questions') selected @endif>Exact
-                                    Match Questions</option> --}}
-                            </select>
-                            <p id="typecheck" style="color: red;">
-                                **Question Type is missing
-                            </p>
-                        </div>
-                        <div class="group-input">
-                            <label for="ques">Question</label>
-                            <input type="text" name="question" id="question" value="{{ old('question') }}">
-                            <p id="questioncheck" style="color: red;">
-                                **Question is missing
-                            </p>
-                        </div>
-                        <div class="group-input single_question-options" id="options-group">
-                            <label for="options">
-                                Options<button type="button" id="optionsbtnadd"><i class="fa-solid fa-plus"></i></button>
-                            </label>
-                            <div class="option-group">
-                                <input type="text" id="option" name="options[]">
-                                <input type="radio" class="answer" name="answer" value="0">
-                            </div>
-                            <div id="optionsdiv"></div>
-                            <p id="optioncheck" style="color: red;">
-                                **Options are missing
-                            </p>
-                        </div>
-
-                        <div class="group-input multi_question-options" id="multi_options-group">
-                            <label for="options">
-                                Options<button type="button" id="multi_optionsbtnadd"><i class="fa-solid fa-plus"></i></button>
-                            </label>
-                            <div class="option-group">
-                                <input type="text" id="option2" name="options[]">
-                                <input type="checkbox" class="answer" name="answer" value="0">
-                            </div>
-                            <div id="multi_optionsdiv"></div>
-                            <p id="optioncheck2" style="color: red;">
-                                **Options are missing
-                            </p>
-                        </div> 
-
-                        <div class="group-input question-answer">
-                            <label for="answer" id="answer-label">
-                                Answer<button type="button" id="answersbtnadd"><i class="fa-solid fa-plus"></i></button>
-                            </label>
-                            <div><small class="text-primary" id="infoQuestion"></small></div>
-                            <input type="text" class="answer" id="answer" name="answers[]" >
-                            <div id="answersdiv"></div>
-                        </div>
-                        <p id="answercheck" style="color: red;">
-                            **Answer is missing
-                        </p>
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" data-bs-dismiss="modal">Cancel</button>
-                        <button type="submit" id="questionsubmitbtn" name="submit" value="save">Save</button>
-                    </div>
-                </form>
-
-            </div>
-        </div>
-    </div> -->
-
-
- \<div class="modal fade" id="add-question-modal">
+  <div class="modal fade" id="add-question-modal">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
 
@@ -312,8 +237,8 @@
                 </div>
 
                 <div class="modal-footer">
-                    <button type="button" data-bs-dismiss="modal">Cancel</button>
-                    <button type="submit" id="questionsubmitbtn" name="submit" value="save">Save</button>
+                    <button type="button" class="btn btn-outline" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-outline" id="questionsubmitbtn" name="submit" value="save">Save</button>
                 </div>
             </form>
 
@@ -370,7 +295,15 @@
     }
 
         // =========================== Toggle Input Fields Using Select Options
-       
+        function handleChange(questionType) {
+    const isSingleSelection = questionType === 'Single Selection Questions';
+    const isMultiSelection = questionType === 'Multi Selection Questions';
+
+    document.getElementById('options-group').style.display = isSingleSelection ? 'block' : 'none';
+    document.getElementById('multi_options-group').style.display = isMultiSelection ? 'block' : 'none';
+    document.getElementById('answer-group').style.display = isSingleSelection || isMultiSelection ? 'block' : 'none';
+}
+
         function handleChange(questionType) {
         const optionsGroup = document.getElementById('options-group');
         const multiOptionsGroup = document.getElementById('multi_options-group');
