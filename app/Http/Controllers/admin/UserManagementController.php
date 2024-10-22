c0e0e679c1b86ab5e3dfb1db1187a2abb19ca8d6
gautamsolankigxp <166195389+gautamsolankigxp@users.noreply.github.com>
2024-10-22 12:12:09 +0530

    role changes

--- app/Http/Controllers/admin/UserManagementController.php
+++ app/Http/Controllers/admin/UserManagementController.php
@@ -260,7 +260,10 @@ class UserManagementController extends Controller
             $q_m_s_roles_name = trim($roleArray[2]);
             // Assuming you have models for q_m_s_divisions and q_m_s_process
             $division = QMSDivision::where('name', $q_m_s_divisions_name)->first();
-            $process = QMSProcess::where('process_name', $q_m_s_processes_name)->first();
+            $process = QMSProcess::where([
+                'process_name' => $q_m_s_processes_name,
+                'division_id' => $division->id
+            ])->first();
             $qmsroles = QMSRoles::where('name', $q_m_s_roles_name)->first();
             $q_m_s_divisions_id = $division->id;
             $q_m_s_processes_id = $process->id;
