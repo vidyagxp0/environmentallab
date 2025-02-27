19f8cca7179d3ff03ef81eb2c1786b06521c2717
NavneetChoudhary-25 <105925355+NavneetChoudhary-25@users.noreply.github.com>
2025-02-27 12:10:06 +0530

    added migration in rca

---
+++ database/migrations/2025_02_27_120732_add_columns_to_root_cause_analyses.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('root_cause_analyses', function (Blueprint $table) {
+            $table->text('more_info_by')->nullable();
+            $table->text('more_info_on')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('root_cause_analyses', function (Blueprint $table) {
+            //
+        });
+    }
+};
