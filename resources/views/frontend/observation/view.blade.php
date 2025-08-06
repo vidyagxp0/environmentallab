c657a3e19ef1839fac76a394bdbd073610b5d61c
SUNILPATELGXP <sunil.patel.gxp@gmail.com>
2025-08-06 17:09:06 +0530

    added reject button

--- resources/views/frontend/observation/view.blade.php
+++ resources/views/frontend/observation/view.blade.php
@@ -96,7 +96,10 @@ $users = DB::table('users')
                                 QA Approval Without CAPA
                             </button>
                             <button class="button_theme1" data-bs-toggle="modal" data-bs-target="#rejection-modal">
-                                Reject CAPA Plan
+                                More Info Required
+                            </button>
+                            <button class="button_theme1" data-bs-toggle="modal" data-bs-target="#cancel-modal">
+                                Reject 
                             </button>
                             {{-- <button class="button_theme1" data-bs-toggle="modal" data-bs-target="#child-modal1">
                                 Child
@@ -126,6 +129,10 @@ $users = DB::table('users')
                     @if ($data->stage == 0)
                         <div class="progress-bars">
                             <div class="bg-danger">Closed-Cancelled</div>
+                        </div>
+                    @elseif ($data->stage == 7)
+                        <div class="progress-bars">
+                            <div class="bg-danger">Closed-Reject</div>
                         </div>
                     @else
                         <div class="progress-bars">
@@ -1396,6 +1403,55 @@ dd($data->date_Response_due2);
                     </div>
                 </form>
 
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="cancel-modal">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">E-Signature</h4>
+                    <!-- <button type="button" class="btn-close" data-bs-dismiss="modal"></button> -->
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal"> <i class="fa fa-times"></i> </button>
+
+                </div>
+
+                <form action="{{ route('rejectObservation', $data->id) }}" method="POST">
+                    @csrf
+                    <!-- Modal body -->
+                    <div class="modal-body">
+                        <div class="mb-3 text-justify">
+                            Please select a meaning and a outcome for this task and enter your username
+                            and password for this task. You are performing an electronic signature,
+                            which is legally binding equivalent of a hand written signature.
+                        </div>
+                        <div class="group-input">
+                            <label for="username">Username <span class="text-danger">*</span></label>
+                            <input type="text" name="username" required>
+                        </div>
+                        <div class="group-input">
+                            <label for="password">Password <span class="text-danger">*</span></label>
+                            <input type="password" name="password" required>
+                        </div>
+                        <div class="group-input">
+                            <label for="comment">Comment <span class="text-danger">*</span></label>
+                            <input type="comment" name="comment"required>
+                        </div>
+                    </div>
+
+                    <!-- Modal footer -->
+                    <!-- <div class="modal-footer">
+                                        <button type="submit" data-bs-dismiss="modal">Submit</button>
+                                        <button>Close</button>
+                                    </div> -->
+                    <div class="modal-footer">
+                        <button type="submit">Submit</button>
+                        <button data-bs-dismiss="modal">Close</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
