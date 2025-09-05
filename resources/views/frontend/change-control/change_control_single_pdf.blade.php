335f5a2ab252fd5807fee800cfe6a84faea0506a
nilesh-2000 <163507962+nilesh-2000@users.noreply.github.com>
2025-09-05 14:09:37 +0530

    update

--- resources/views/frontend/change-control/change_control_single_pdf.blade.php
+++ resources/views/frontend/change-control/change_control_single_pdf.blade.php
@@ -177,10 +177,9 @@
                     <strong>Change Control No.</strong>
                 </td>
                 <td class="w-40">
-                    {{ Helpers::getDivisionName($data->division_id) }}/CC/{{ date('Y') }}/{{ $data->record ? str_pad($data->record, 4, '0', STR_PAD_LEFT) : '' }}
-                </td>
+                  {{ Helpers::getDivisionName($data->division_id) }}/CC/{{ date('Y') }}/{{ $data->record_number ? str_pad($data->record_number->record_number, 4, '0', STR_PAD_LEFT) : '001' }}                </td>
                 <td class="w-30">
-                    <strong>Record No.</strong> {{ str_pad($data->record, 4, '0', STR_PAD_LEFT) }}
+                    <strong>Record No.</strong> {{ $data->record_number ? str_pad($data->record_number->record_number, 4, '0', STR_PAD_LEFT) : '001' }}
                 </td>
             </tr>
         </table>
